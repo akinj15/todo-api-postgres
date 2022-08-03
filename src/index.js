ee33f141d32d6e096a0090558f31e2fd05115f68
akin <akin.bezerra123@gmail.com>
2022-08-02 22:21:17 -0300

    att

--- src/index.js
+++ src/index.js
@@ -69,7 +69,7 @@ app.patch('/user/:user_id', async (req, res) => {
 })
 
 app.delete('/user/:user_id', async (req, res) => {
-    const { user_id } = req.params
+    let { user_id } = req.params
     try {
         const deletUser = await pool.query('DELETE FROM users WHERE user_id = ($1) RETURNING *', [user_id])
         if (!deletUser.rows[0]) {
